f72a83600627297d7a7553bcbf52f3de688e8991
wilbur <wkc120796@163.com>
2021-06-17 17:54:39 +0800

    商城完善

--- pages/shop-detail/shop-detail.vue
+++ pages/shop-detail/shop-detail.vue
@@ -17,7 +17,7 @@
 				<view class="item-cp">{{titleTag}}</view>
 				<view class="item-title">{{item.goodsName}}</view>
 			</view>
-			<view class="item-coupon-wrap" :style="'margin-top:'+couponTop+'px;'" v-if="item.discount"  @click="mallTurnChain">
+			<view class="item-coupon-wrap" :style="'margin-top:'+couponTop+'px;'" v-if="item.discount>0"  @click="mallTurnChain">
 				<image style="width: 100%;" src="../../static/shop/coupon.png" mode="widthFix"></image>
 				<view class="item-coupon-price">{{numFilter(item.discount)}}</view>
 				<view class="item-coupon-time">{{$u.timeFormat(item.couponStartTime, 'yyyy.mm.dd')}}-{{$u.timeFormat(item.couponEndTime, 'yyyy.mm.dd')}}</view>
@@ -64,7 +64,113 @@
 </template>
 
 
-<script src="./shop-detail.js"></script>
+<script>
+	import {request} from "../../utils/request";
+	export default {
+			data() {
+				return {
+					item: {},
+					queryParams: {},
+					titleTag: '拼多多',
+					couponTop: 35,
+					collapseHeadStyle: {
+						"padding-left": "26rpx",
+						"background-color": "#ffffff",
+					}
+				};
+			},
+			onLoad(opt) {
+				this.queryParams = opt;
+				this.getDetail();
+				this.initTitleTag();
+			},
+			methods: {
+				numFilter(value) {
+					if (0 == value || '' == value || undefined == value) {
+						return 0;
+					}
+					const fenStr = '00' + Number.parseInt(value.toString()).toString()
+					return Number.parseFloat(fenStr.replace(/^(\d+?)(\d{2})$/g, '$1.$2'))
+				},
+				async getDetail(){
+					const that = this;
+					const result = await request({url: '/program/mall/mallDetail'+this.$u.queryParams(this.queryParams), method: 'GET'});
+					if (result.code == 200) {
+						this.item = result.data;
+						
+						// 从新初始话折叠面板
+						that.$nextTick(() => {
+							that.$refs.collapseView.init();
+							
+							// 获取标题高度
+							let info = uni.createSelectorQuery().select(".item-title");
+					　　　  　info.boundingClientRect(function(data) { //data - 各种参数
+							  // 96  60  -36
+							  // 72  40  -32
+							  // 48  15  -33
+							  // 24  0  -24
+							  if(data.height < 48){
+								  that.couponTop = 0;
+							  }else if(data.height >= 48 && data.height < 72){
+								  that.couponTop = 15;
+							  }else if(data.height >= 72 && data.height < 96){
+								  that.couponTop = 40;
+							  }else if(data.height >= 96){
+								  that.couponTop = 60;
+							  }
+					　　    }).exec()
+						})
+						
+					}else{
+						this.$refs.uToast.show({
+							title: '无效商品！',
+							type: 'error',
+							// url: '/pages/shopping/shopping',
+							back: true,
+							isTab: true,
+							position: 'top'
+						})
+						
+					}
+				},
+				async mallTurnChain(){
+					uni.showLoading({
+						title:"加载中..."
+					})
+					const result = await request({url: '/program/mall/mallTurnChain'+this.$u.queryParams(this.queryParams), method: 'GET'});
+					uni.hideLoading();
+					if (result.code == 200) {
+						const weapp = result.data.weChatInfo;
+						wx.navigateToMiniProgram({
+							appId: weapp.app_id,
+							path: weapp.page_path,
+							success(res) {
+								// 打开成功
+							}
+						})
+					}
+				},
+				toShopIndex(){
+					wx.redirectTo({
+						url: '/pagesB/pages/shopping/shopping'
+					})
+				},
+				initTitleTag(){
+					switch(this.queryParams.cpType){
+						case 'pdd':
+						this.titleTag = '拼多多';
+						break;
+						case 'jd':
+						this.titleTag = '京东';
+						break;
+						case 'wph':
+						this.titleTag = '唯品会';
+						break;
+					}
+				}
+			}
+		}
+</script>
 <style>
 	/* page不能写带scope的style标签中，否则无效 */
 	page {
